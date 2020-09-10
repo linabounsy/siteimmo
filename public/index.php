a602c7ccbb3e677f4c4ff4806fbfdc9abac64045
linabounsy <49026538+linabounsy@users.noreply.github.com>
2020-09-10 18:20:14 +0200

    fix : connexion admin depuis le template
    ajout du lien de connexion a l'admin

--- public/index.php
+++ public/index.php
@@ -2,16 +2,36 @@
 session_start();
 
 use Controller\HomePage;
+use Controller\AdminBoard;
 
 require('../controller/HomePage.php');
+require('../controller/AdminBoard.php');
 
-$homepage = new homePage();
+$homepage = new HomePage();
+$admin = new AdminBoard();
 
 try { // On essaie de faire des choses 
     if (isset($_GET['action'])) {
-       // conditions
+        if ($_GET['action'] == 'connexion') {
+            $admin->adminConnexion();
+        }
+        if ($_GET['action'] == 'indexadmin') {
+            $admin->indexAdmin();
+        }
+        if ($_GET['action'] == 'displayclient') {
+            $admin->displayClients();
+        }
+        if ($_GET['action'] == 'insertnewclient') {
+            $admin->insertNewClient();
+        }
+        if ($_GET['action'] == 'newadvert') {
+            $admin->addAdvert();
+        }
+        if ($_GET['action'] == 'insertnewadvert') {
+            $admin->insertNewAdvert();
+        }
     } else {
-        $homepage->homePage();
+        $homepage->index();
     }
 } catch (Exception $e) { // S'il y a eu une erreur, alors...
     echo 'Erreur : ' . $e->getMessage();
